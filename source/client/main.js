ad50964f7e4d5dcdc6a2ee0a9aedc1133d764666
InvertedOwl <70726294+InvertedOwl@users.noreply.github.com>
2022-07-18 16:43:49 -0600

    init

---
+++ source/client/main.js
@@ -0,0 +1,188 @@
+const socket = io(); 
+
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('id');
+const id = Math.floor(Math.random() * 100000)
+let color = "#000000";
+let mouseColor = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%, 0.5"
+var code;
+
+if (myParam != null) {
+    socket.emit("join", myParam);
+} else {
+    socket.emit("create")
+    socket.on("create_res", (res) => {
+        code = res.id;
+        window.history.pushState("object or string", "Title", "/?id=" + res.id);
+    })
+}
+socket.on("strokes", (strokes) => {
+    incomingStrokes[strokes.id] = strokes.strokes;
+
+})
+socket.on("mouse", (mouse) => {
+    mice[mouse.id] = mouse;
+
+})
+
+socket.on("reset", () => {
+    strokes.splice(0, strokes.length);
+    incomingStrokes = {};
+})
+
+
+
+var ctx = document.getElementById("main").getContext("2d");
+
+function draw() {
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.lineWidth = 15;
+    ctx.lineCap = 'round';
+
+    for (let strokee of Object.keys(incomingStrokes)) {
+        if (strokee == id) {
+            continue;
+        }
+        strokee = incomingStrokes[strokee];
+        for (let stroke of strokee) {
+            for (let i = 1; i < stroke.length; i++) {
+                let p = stroke[i - 1];
+                let c  = stroke[i];
+                ctx.strokeStyle = p[2]
+                ctx.beginPath()
+                ctx.moveTo(p[0], p[1])
+                ctx.lineTo(c[0], c[1])
+                ctx.stroke()
+            }
+        }
+    }
+
+    for (let stroke of strokes) {
+        for (let i = 1; i < stroke.length; i++) {
+            let p = stroke[i - 1];
+            let c  = stroke[i];
+            ctx.strokeStyle = p[2]
+            ctx.beginPath()
+            ctx.moveTo(p[0], p[1])
+            ctx.lineTo(c[0], c[1])
+            ctx.stroke()
+        }
+    }
+
+
+    for (let i = 1; i < currentStroke.length; i++) {
+        let p = currentStroke[i - 1];
+        let c  = currentStroke[i];
+        ctx.strokeStyle = p[2]
+        ctx.beginPath()
+        ctx.moveTo(p[0], p[1])
+        ctx.lineTo(c[0], c[1])
+        ctx.stroke()
+    }
+
+    for (let mouseKey of Object.keys(mice)) {
+        if (mouseKey == id) {
+            continue;
+        }
+        let mouse = mice[mouseKey].mouse;
+
+        ctx.beginPath();
+        ctx.arc(mouse[0], mouse[1], 8, 0, 2 * Math.PI, false);
+        ctx.fillStyle = mice[mouseKey].color;
+        ctx.fill();
+        // ctx.lineWidth = 5;
+        // ctx.strokeStyle = '#003300';
+        // ctx.stroke();
+    }
+    // ctx.fillRect(50, 50, 50, 50)
+}
+// draw();
+setInterval(draw, 16)
+
+let strokes = []
+let currentStroke = []
+let undoneStrokes = []
+let incomingStrokes = {};
+let mice = {};
+
+// MOUSE STUFF
+let held = false;
+document.body.addEventListener("mousedown", (e) => {
+    held = true;
+})
+
+let pX = null;
+let pY = null;
+document.body.addEventListener("mousemove", (e) => {
+
+    if (held) {
+        currentStroke.push([e.clientX, e.clientY, color])
+    }
+
+    socket.emit("mouse", {
+        "mouse":[e.clientX, e.clientY],
+        "id":id,
+        "color": mouseColor
+        })
+})
+
+document.body.addEventListener("mouseup", (e) => {
+    held = false;
+    let pX = null;
+    let pY = null;
+    strokes.push([...currentStroke]);
+    currentStroke.splice(0, currentStroke.length);
+    socket.emit("strokes", {
+        "strokes" : strokes,
+        "id" : id
+    });
+})
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'z') {
+        undoneStrokes.push(strokes.pop());
+        socket.emit("strokes", {
+            "strokes" : strokes,
+            "id" : id
+        });
+    }
+    if (event.ctrlKey && event.key === 'y') {
+        strokes.push(undoneStrokes.pop());
+        socket.emit("strokes", {
+            "strokes" : strokes,
+            "id" : id
+        });
+    }
+});
+
+window.onbeforeunload = function () {
+    socket.emit("mouse", {
+        "mouse":[-100000, -100000],
+        "id":id,
+        "color": mouseColor
+        })
+}
+
+document.getElementById("colorPick").addEventListener("change", (e) => {
+    color = e.target.value;
+})
+
+function reset() {
+    socket.emit("reset")
+}
+
+
+window.addEventListener("beforeunload", function (e) {
+    console.log(socket);
+    socket.emit("mouse", {
+        "mouse":[-500, -500],
+        "id":id,
+        "color": mouseColor
+        })
+    // var confirmationMessage = 'It looks like you have been editing something. '
+    //                         + 'If you leave before saving, your changes will be lost.';
+
+    // (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+    // return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+});
---
+++ source/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const crypto = require('crypto');
+const port = 3007
+app.use('/static',  express.static(__dirname + "/client"))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/client/index.html")
+})
+
+http.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+let mice = {};
+
+io.on('connection', (socket) => {
+  socket.on("join", (msg) => {
+    socket.join(msg);
+  })
+
+  socket.on("strokes", (strokes) => {
+    for (let i of socket.rooms) {
+      io.to(i).emit("strokes", strokes)
+    }
+  })
+  socket.on("mouse", (strokes) => {
+    for (let i of socket.rooms) {
+      io.to(i).emit("mouse", strokes)
+      mice[strokes.id] = strokes;
+    }
+  })
+
+  socket.on("reset", () => {
+    for (let i of socket.rooms) {
+      io.to(i).emit("reset")
+    }
+  })
+
+  socket.on("create", () => {
+    let id = createId(64);
+    socket.emit("create_res", {"id": id})
+    for (let i of socket.rooms) {
+      socket.leave(i);
+    }
+    socket.join(id);
+    console.log(socket.rooms);
+  })
+
+  socket.on("disconnect", (reason) => {
+    for (let mouseID of Object.keys(mice)) {
+      let mouse = mice[mouseID];
+      mouse.color = "hsl(0, 100%, 50%, 0)";
+      console.log(mouse);
+      io.emit("mouse", mouse)
+    }
+
+
+  });
+})
+
+function createId(length) {
+  let token = crypto.randomBytes(length);
+  token = token.toString('hex');
+  return token;
+}
